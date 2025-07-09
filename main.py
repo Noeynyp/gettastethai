4c3533e32cd615817ea5132a0ca7166155608706
MaxDPK <Max.khemanukul@gmail.com>
2025-07-09 13:58:18 +0700

    add main.py for Azure deploy

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import os
+
+app = FastAPI()
+
+# Serve static files from the 'dist' folder (Vite build output)
+app.mount("/", StaticFiles(directory="dist", html=True), name="static")
